ad27fdf49d985efa0ba3b44f84c17e288d7ada3f
sn-nast <sunsun.natchanon@gmail.com>
2021-08-28 13:14:11 +0700

    Weekly 4

--- Weekly Assignment/Week_04_sol2.c
+++ Weekly Assignment/Week_04_sol2.c
@@ -6,22 +6,20 @@ int main(){
     int n = 10, c;
     float x[10], sum_x , xbar, sd, diff, diffSqa, sum_diffSqa;
    
-    printf("Find (sample) standard devision., please input ten Numbers only.\n\n");
-    for (c = 1; c <= n; c++){
-         //input
-        printf("input data %d : ", c);
+    printf("Find (sample) standard devision., please enter ten Numbers only.\n\n");
+    for (c = 0; c < n; c++){
+    //input
+        printf("Enter data %d : ", c+1);
         scanf("%f", &x[c]);
-        // Find SD
+    // Find SD
         xbar += x[c]/n*1.0;
-        sum_x += x[c];
-        if (c == n){
-            for (c = 1; c < n; c++){
-                sum_diffSqa += (x[c] - xbar)*(x[c] - xbar);
-            }
-        }
     }
-    // output
+    for (c = 0; c < n; c++){
+        sum_diffSqa += pow(x[c] - xbar, 2);
+    }
     sd = sqrt(sum_diffSqa/(n-1));
+   
+    // output
     printf("\nSample standard devision = %f", sd);
 
     return 0;
