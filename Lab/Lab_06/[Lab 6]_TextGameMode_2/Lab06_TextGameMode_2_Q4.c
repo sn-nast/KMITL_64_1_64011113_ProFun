3225d90bb2724427bab06b7f03976e38f0378488
sn-nast <sunsun.natchanon@gmail.com>
2021-09-13 22:00:24 +0700

    Lab 6 TextModeGame 2 Finish

--- Lab/Lab_06/[Lab 6]_TextGameMode_2/Lab06_TextGameMode_2_Q4.c
+++ Lab/Lab_06/[Lab 6]_TextGameMode_2/Lab06_TextGameMode_2_Q4.c
@@ -36,9 +36,9 @@ void draw_ship(int x, int y)
 	printf(" ");
 }
 
+//void bulletMove(int x; )
+
 void draw_bullet(int x, int y) {
-	//x = x + 2;
-	//y = y - 1;
 	gotoxy(x, y);
 	setcolor(6, 0);
 	printf("!!!");		// bulletLen = 3
@@ -46,44 +46,53 @@ void draw_bullet(int x, int y) {
 }
 
 void erase_bullet(int x, int y) {
-	//x = x + 2;
-	//y = y - 1;
 	gotoxy(x, y);
 	printf("   ");		// bulletLen = 3
 }
 
+
+
+
 int main() {
 	setcursor(0);
 	char ch = '.';
-	int x = 38, y = 20, shipLen = 7;
-	int maxWidth = 80;
+
+	int x = 38, y = 30, shipLen = 7;
+	int maxWidth = 189;
 	char direction = ' ';
-	int bullet  = 0;		// no bullet = 0
-	//int bullet_remain = 5;
-	int xb[5], yb[5];
-	//int* ptr_bullet;
+
+	int xbl[6], ybl[6];
+	int bullet = 0;		// no bullet = 0
+	int bulletRe = 5, bulletAdd = 0, bulletRun = 0;
+	int bulletNo[5] = { 0, 0, 0, 0, 0 };
+	int* pt_bulletNo = bulletNo;
+
 	draw_ship(x, y);
 	do {
-
 		if (_kbhit()) {
 			ch = _getch();
-			if (ch == 'a') {
+			if (ch == 'a' || ch == 'A') {
 				if (x <= 0) { x = 0; }
 				else {direction = 'L';}
 			}
-			if (ch == 'd') {
+			if (ch == 'd' || ch == 'D') {
 				if (x >= maxWidth - shipLen) { x = maxWidth - shipLen; }
 				else {direction = 'R';}
 			}
-			if (ch == 's') {direction = ' ';}
-			if (bullet < 5 && ch == ' ') {
+			if (ch == 's' || ch == 'S') {direction = ' ';}
+ 			if (bullet < 5 && ch == ' ') {
 				bullet++;
-				xb[bullet - 1] = x + 2;		// 1/3 of ship = 2 
-				yb[bullet - 1] = y - 1;
+				for (int i = 1; i <= 5; i++) {
+					if (bulletNo[i - 1] != 1 ) {
+						xbl[i - 1] = x + 2; // 1/3 of ship = 2 
+						ybl[i - 1] = y - 1;
+						break;
+					}
+				}
 			}
 			fflush(stdin);
-
 		}
+		// Auto move
 		if (x <= 0) { direction = 'R'; }
 		if (x >= maxWidth - shipLen) { direction = 'L'; }
 		if (direction != ' ') {
@@ -97,18 +106,40 @@ int main() {
 			}
 		}
 
-		if (bullet != 0) {
-			int bullet2 = bullet;
-			//for (int c = 1; c-1 < bullet; c++) {
-			//	erase_bullet(xb[c-1], yb[c-1]);
-			//	if (yb[c-1] == 1) { bullet--; }
-			//	else draw_bullet(xb[c-1], --yb[c -1]);
-			//}
-			erase_bullet(xb[bullet-1], yb[bullet-1]);
-			if (yb[bullet-1] == 1) { bullet--; }
-			else draw_bullet(xb[bullet-1], --yb[bullet -1]);
-
+		if (bulletRun < bullet) { bulletAdd = bullet - bulletRun ; }
+		if (bullet != 0 || bulletRun != 0) {
+			// have bullet add
+       		bulletRun += bulletAdd;
+			if (bulletAdd != 0) {
+				int c_bulletAdd;
+				if (bulletRun == bulletAdd) { c_bulletAdd = bulletAdd; }
+				else if (bulletRun > bulletAdd) { c_bulletAdd = bulletAdd + 1; }
+				for (int c = 1; c <= bulletAdd ; c++) {
+					for (int i = 1; i <= 5; i++) {
+						if (bulletNo[i - 1] != 1) {
+							bulletNo[i - 1] = 1;
+							bulletAdd--;
+							if (bulletAdd == 0) { break; }
+						}
+					}
+				}
+			}
+			// Bullet move (check bullet must run)
+			for (int i = 1; i <= 5; i++) {
+				// Have remain in bulletNo[1 - 5]?
+    			if (bulletNo[i - 1] == 1) {
+					erase_bullet(xbl[i - 1], ybl[i - 1]);
+					if (ybl[i - 1] == 0) { 
+						if(bullet > 0){
+							bullet--;
+							bulletRun--;
+						}
+						bulletNo[i - 1] = 0;
+					}
+					else draw_bullet(xbl[i- 1], --ybl[i - 1]);
+				}
+			}
 		}
 		Sleep(10);
-		} while (ch != 'x');
-}
+	} while (ch != 'x'); 
+}  
--- WorkingTest.c
+++ WorkingTest.c
@@ -1,18 +1,29 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
-  int n = 6;
-  // for (int c =1 ; c < 10; c++){
-    int randCount = rand()%(n) + 1;
-  //   printf("1: %d\n", randCount);
 
-  //   while (/* condition */)
-  //   {
-  //     /* code */
-  //   }
-  //    (randCount == 0)  {
-  //   printf("%d\n", randCount);
-  //   }
-  // // }
+  int x = 10 ,y = 20 , z = 30 ,*p1 ,*p2;
+  p1 = &x;
+  p2 = &y;
+  printf("%d %d %d %d %d\n", x, y, z, *p1, *p2);
+
+  x = 80;
+  printf("%d %d %d %d %d\n", x, y, z, *p1, *p2);
+
+  p1 = &z;
+  p2 = p1 ;
+  printf("%d %d %d %d %d\n", x, y, z, *p1, *p2);
+
+  *p1 = 100;
+  printf("%d %d %d %d %d\n", x, y, z, *p1, *p2);
 
 }
+// 6684160
+// 1
+// 6684152
+// 6684160
+
+// 6684160
+// 1
+// 6684152
+// 6684160
