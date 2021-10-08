f13ad65ec1bd8fc90ba8eff6090ea794151b257f
sn-nast <sunsun.natchanon@gmail.com>
2021-10-08 19:20:56 +0700

    Weekly 9 completed

--- Weekly Assignment/Week_09_sol2.cpp
+++ Weekly Assignment/Week_09_sol2.cpp
@@ -1,25 +1,56 @@
 // Level 4
 // 64011113
 /* 63.	จงเขียนโปรแกรม Copy ไฟล์ข้อมูลโดยให้นักศึกษาป้อนข้อมูลของไฟล์ต้นทาง และข้อมูลไฟล์ปลายทาง */
-// Sol 2
+// Sol 1
 
 #include<stdio.h> 
+#include<string.h>
 
 int main(){
-    FILE *inputFile_ptr, *outputFile_ptr;
-    char inputFile_path[100], outputFile_path[100];
-    // input File path
-    printf("input\t: ");
-    scanf("%[^\n]%*c", inputFile_path);
-    printf("output\t: ");
-    scanf("%[^\n]%*c", outputFile_path);
+    struct _File {
+        FILE *ptr;
+        char path[BUFSIZ];
+        int sum = 0;
+    };
+    _File inputFile[5], outputFile;
 
-    // copy file
-    char ch;
-    inputFile_ptr = fopen(inputFile_path, "r");
-    outputFile_ptr = fopen(outputFile_path, "w"); 
-    while((ch = fgetc(inputFile_ptr)) != EOF) {fputc(ch, outputFile_ptr);}
-    fclose(inputFile_ptr);
-    fclose(outputFile_ptr);
+    // input file path
+    strcpy(inputFile[0].path, "a.txt");
+    strcpy(inputFile[1].path, "c.txt");
+
+    // Add input file path
+    // strcpy(inputFile[0].path, "....") ....
+
+    // output file path
+    strcpy(outputFile.path, "b.txt");
+
+    int sumAll;
+    for(int i = 0; i < sizeof(inputFile)/sizeof(_File); i++){
+        if(strstr(inputFile[i].path, ".txt") == NULL) {break;} 
+
+        inputFile[i].ptr = fopen(inputFile[i].path, "r");
+        printf("File %s\n", inputFile[i].path );                // Check status
+        printf("number\t: ", inputFile[i].path );               // Check status
+        while(!feof(inputFile[i].ptr)){
+            int num;
+            fscanf(inputFile[i].ptr, "%d", &num);
+            printf("%d ", num);                                 // Check status
+            inputFile[i].sum += num;
+        }
+        // Sum of this file
+        printf("\nsum\t: %d\n", inputFile[i].sum);              // Check status
+        sumAll += inputFile[i].sum;
+
+        printf("sumAll\t: %d\n\n", sumAll);                     // Check status
+    }
+
+    outputFile.ptr = fopen(outputFile.path, "w");
+    fprintf(outputFile.ptr, "%d", sumAll);
+
+    fclose(outputFile.ptr);
+
+    for (int i = 0; i < sizeof(inputFile)/sizeof(_File); i++) { 
+        fclose(inputFile[i].ptr);
+    }
     return 0;
 }
